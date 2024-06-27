5217b8eb93d43d9cb851500899271808489c85a6
dineshchintu <musuku.dinesh@practo.com>
2024-06-27 14:05:50 +0530

    files added

---
+++ src/main/java/org/example/service/UserService.java
@@ -0,0 +1,14 @@
+package org.example.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public void getInfo() {
+        System.out.println("Fetching user information...");
+        // Additional logic to fetch user information
+    }
+
+    // Other methods related to user operations
+}
